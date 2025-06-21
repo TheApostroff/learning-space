fb7e079c167598a4c1f36988b11fe5e825e4f4ba
Adrian Popov <qwasuss@gmail.com>
2025-06-21 22:43:01 +0300

    Add userservice

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,26 +1,35 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Port   string `env:"PORT" env-default:"8080"`
+	Port   string `yaml:"port" env:"PORT" env-default:"8080"`
 	Server Server `yaml:"server"`
 }
 
 type Server struct {
 	Port     string `yaml:"port" env:"PORT" env-default:"8080"`
 	Host     string `yaml:"host" env:"HOST" env-default:"localhost"`
-	User     string `yaml:"user" env:"USER" env-default:"postgres"`
+	User     string `yaml:"user" env:"DB_USER" env-default:"postgres"`
 	Password string `yaml:"password" env:"PASSWORD" env-default:"postgres"`
 	Database string `yaml:"database" env:"DATABASE" env-default:"postgres"`
 }
 
-func NewConfig() (*Config, error) {
-	cfg := &Config{}
-	if err := cleanenv.ReadConfig("config.yml", cfg); err != nil {
-		return nil, err
+func NewConfig() *Config {
+	cfg := Config{}
+
+	err := cleanenv.ReadConfig(fmt.Sprintf("%s/config/%s", os.Getenv("PWD"), "config.yaml"), &cfg)
+	if err != nil {
+		log.Fatalf("Configuration error: %v", err)
 	}
-	return cfg, nil
+
+	log.Printf("Configuration: %+v", cfg)
+
+	return &cfg
 }
---
+++ backend/internal/controller/user.go
@@ -0,0 +1,101 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/TheApostroff/skill-space/internal/models"
+	"github.com/TheApostroff/skill-space/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	service *service.UserService
+}
+
+func NewUserController(service *service.UserService) *UserController {
+	return &UserController{service: service}
+}
+
+func (c *UserController) CreateUser(ctx *gin.Context) {
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.service.CreateUser(&user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, user)
+}
+
+func (c *UserController) LoginUser(ctx *gin.Context) {
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u, err := c.service.LoginUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	ctx.JSON(http.StatusOK, u)
+}
+
+func (c *UserController) GetUser(ctx *gin.Context) {
+	id, err := strconv.ParseUint(ctx.Param("id"), 10, 32)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	user, err := c.service.GetUser(uint(id))
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (c *UserController) UpdateUser(ctx *gin.Context) {
+	id, err := strconv.ParseUint(ctx.Param("id"), 10, 32)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user.ID = uint(id)
+	if err := c.service.UpdateUser(&user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
+
+func (c *UserController) DeleteUser(ctx *gin.Context) {
+	id, err := strconv.ParseUint(ctx.Param("id"), 10, 32)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	if err := c.service.DeleteUser(uint(id)); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
+}
