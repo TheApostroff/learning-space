7e701047d958d94c37d0d4bb2ae5e65ebb90980b
Adrian Popov <qwasuss@gmail.com>
2025-06-21 15:30:53 +0300

    User model and schemas

---
+++ app/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    full_name = Column(String, nullable=True)
+    hashed_password = Column(String, nullable=False)
